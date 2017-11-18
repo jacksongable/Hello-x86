dfc560859ca8d73d2b7c3dc1a1e85ff0ef0b81a7
jacksongable <jackson.r.gable@gmail.com>
2017-11-18 01:57:04 -0500

    Better spacing and added copyright

--- hello_world.asm
+++ hello_world.asm
@@ -1,23 +1,8 @@
-section .text	; the section of the program containing executable instructions
+;
+; Copyright (c) 2017 Jackson Gable
+; This software is released under the MIT License.
+;
 
-	global _start
-
-	_start:
-		
-		mov eax, 0x4			; set the system call to 'sys_write'
-		mov ebx, 0x1			; set stdout (the console) as the output
-		mov ecx, msg			; move the message ('Hello world!') to register ECX
-		mov edx, len			; move the length of the message to register EDX
-		
-		
-		int K_CALL			; fire interrupt to notify kernel to execute system call in register EAX 
-
-
-		mov eax, 0x1			; set the system call to 'sys_exit'
-		mov ebx, 0x0			; set the exit code to '0' to indicate normal termination
-		
-
-		int K_CALL			; fire interrupt to notify kernel to execute system call in register EAX
 
 section .data	; the section of the program containing initialized data
 
@@ -28,3 +13,23 @@ section .data	; the section of the program containing initialized data
 		K_CALL	equ	0x80			; the software interrupt to fire. Interrupt 0x80 notifies the Linux kernel to 
 							; execute the system call set in register EAX using the arguments 
 							; found in other registers as necessary
+
+section .text	; the section of the program containing executable instructions
+
+	global _start
+
+	_start:
+		
+		mov eax, 0x4			; set the system call to 'sys_write'
+		mov ebx, 0x1			; set stdout as the file descriptor (a.k.a. the output)
+		mov ecx, msg			; move the message ('Hello world!') to register ECX
+		mov edx, len			; move the length of the message to register EDX
+				
+		int K_CALL			; fire interrupt to notify kernel to execute system call in register EAX 
+
+		mov eax, 0x1			; set the system call to 'sys_exit'
+		mov ebx, 0x0			; set the exit code to '0' to indicate normal termination
+		
+		int K_CALL			; fire interrupt to notify kernel to execute system call in register EAX
+
+
